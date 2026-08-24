24a1f1cadc65e6fcc5598304216de54b545d1f86
vinoth-techyyy <vinothdevops87@gmail.com>
2026-08-24 20:58:36 +0530

    Update next.config.ts for GitHub Pages
    Configure Next.js for GitHub Pages deployment.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  // Your GitHub repository name
+  basePath: "/portfolio",
+
+  // Required if you use next/image with GitHub Pages
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
 };
 
 export default nextConfig;
